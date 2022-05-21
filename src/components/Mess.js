e30e8bca2f41f1120fbc1c5bfc5c18d4ce730b34
chicken <dangconglan2k@gmail.com>
2022-05-21 23:15:33 +0700

    update

--- src/components/Mess.js
+++ src/components/Mess.js
@@ -7,7 +7,7 @@ export class Mess extends Component {
   render() {
     return (
       <div>
-        <div id="fb-root"></div>
+        {/* <div id="fb-root"></div> */}
         {/* <div id="fb-customer-chat" class="fb-customerchat"></div> */}
       </div>
     );
